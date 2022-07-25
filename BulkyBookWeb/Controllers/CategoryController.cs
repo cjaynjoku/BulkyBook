f469f5b8989c8b87ce04ff20d5852ada2776aba0
Januarius-Njoku <76001800+Januarius-Njoku@users.noreply.github.com>
2022-07-25 10:27:02 +0100

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,118 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly BulkyDBContext _db;
+
+        public CategoryController(BulkyDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        // GET
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "The Display order cannot match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+           
+
+        }
+
+        // GET
+        [HttpGet]
+
+        public IActionResult Edit(int? Id)
+        {
+            if(Id== null || Id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "The Display order cannot match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        // GET
+        [HttpGet]
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? Id)
+        {
+            var categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ BulkyBookWeb/Data/BulkyDBContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class BulkyDBContext:DbContext
+    {
+        public BulkyDBContext(DbContextOptions<BulkyDBContext> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
